e9e6f657818672ec80c3fee140154484be00c107
Aaron-KASONGO <66473473+Aaron-KASONGO@users.noreply.github.com>
2023-03-12 15:24:25 +0000

    Impolementation of radiobutton

--- react-mui-demo/src/components/MuiRadioButton.js
+++ react-mui-demo/src/components/MuiRadioButton.js
@@ -1,10 +1,16 @@
-import { Box, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material"
-
+import { Box, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, FormHelperText } from "@mui/material"
+import { useState } from "react";
 
 export const MuiRadioButton = () => {
+    const [years, setYears] = useState('');
+
+    const handleYearsChange = (event) => {
+        setYears(event.target.value)
+    }
+
   return (
     <Box>
-        <FormControl>
+        <FormControl error={!years}>
             <FormLabel
             id="job-experience-group-label"
             >
@@ -13,11 +19,15 @@ export const MuiRadioButton = () => {
             <RadioGroup
             name="job-experience-group"
             aria-labelledby="job-experience-group-label"
+            value={years}
+            onChange={handleYearsChange}
+            row
             >
-                <FormControlLabel control={<Radio />} label="0-2" value="0-2" />
+                <FormControlLabel control={<Radio size="small" color="secondary" />} label="0-2" value="0-2" />
                 <FormControlLabel control={<Radio />} label="0-4" value="0-4" />
                 <FormControlLabel control={<Radio />} label="0-4" value="0-10" />
             </RadioGroup>
+            <FormHelperText>Invalid selection</FormHelperText>
         </FormControl>
     </Box>
   )
